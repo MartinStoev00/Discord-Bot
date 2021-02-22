201f910e5bd74853c425823a0c110ad78123c075
MartinStoev00 <martinstoev00@gmail.com>
2021-02-22 14:14:53 +0200

    1

--- src/bot.js
+++ src/bot.js
@@ -1,30 +1,34 @@
-require("dotenv").config();
-const { Client } = require("discord.js");
-const vote = require("./vote");
-const ping = require("./ping");
-const PREFIX = "$";
-let personVoted = [];
-const client = new Client();
+try {
+  require("dotenv").config();
+  const { Client } = require("discord.js");
+  const vote = require("./vote");
+  const ping = require("./ping");
+  const PREFIX = "$";
+  let personVoted = [];
+  const client = new Client();
 
-setInterval(() => {
-  personVoted = [];
-}, 180000);
+  setInterval(() => {
+    personVoted = [];
+  }, 180000);
 
-client.on("message", (message) => {
-  if (message.author.bot) return;
-  if (message.content.startsWith(PREFIX)) {
-    const [CMD_NAME, ...args] = message.content
-      .trim()
-      .substring(PREFIX.length)
-      .split(/\s+/);
-    switch (CMD_NAME) {
-      case "vote":
-        vote(args, message, personVoted);
-      case "ping":
-        ping(args, message);
+  client.on("message", (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+      const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+      switch (CMD_NAME) {
+        case "vote":
+          vote(args, message, personVoted);
+        case "ping":
+          ping(args, message);
+      }
     }
-  }
-});
+  });
 
-client.login(process.env.DISCORD_BOT);
-//a
+  client.login(process.env.DISCORD_BOT);
+  //a
+} catch (err) {
+  console.log(err);
+}
